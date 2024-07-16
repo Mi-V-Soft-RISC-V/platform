8a2f084cd36f771a6ff33e0b19f3f20e5171f07d
Rohitkumar Girase <Rohit.Girase@microchip.com>
2024-07-16 14:48:34 +0530

    Update ordering of direct interrupt handlers in local IRQ table
    In direct interrupt mode, irq handlers are called via the
    local_irq_handler_table. The ordering of the handlers was not
    in line with the mie register and was causing some interrupts to
    be handeled incorrectly.
    
    Jira-Id: ESSBM-253
    Signed-off-by: Rohitkumar Girase <Rohit.Girase@microchip.com>

--- miv_rv32_hal/miv_rv32_hal_version.h
+++ miv_rv32_hal/miv_rv32_hal_version.h
@@ -18,7 +18,7 @@ extern "C" {
 
 #define MIV_RV32_HAL_VERSION_MAJOR 4
 #define MIV_RV32_HAL_VERSION_MINOR 3
-#define MIV_RV32_HAL_VERSION_PATCH 101
+#define MIV_RV32_HAL_VERSION_PATCH 102
 
 #ifdef __cplusplus
 }
